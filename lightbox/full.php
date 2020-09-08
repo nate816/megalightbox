da42c9da53ff6864dd4f9d181eb9c7fcc8c3b234
Nate Fazakerly <nate@natefaz.com>
2020-09-08 13:38:22 -0700

    file permissions

--- lightbox/full.php
+++ lightbox/full.php
@@ -1,6 +1,11 @@
 <?php
 $fullSizeArray = array();
 $filePath = $_GET['filePath'];
+//update file permissions to delete/create images etc...
+chmod($filePath, 0755);
+chmod($filePath . 'thumbs.php', 0644);
+chmod($filePath . 'full.php', 0644);
+chmod($filePath . 'captions.php', 0644);
 
 foreach (array_filter(glob('images/fullsize/*'), 'is_file') as $data)
                 {
